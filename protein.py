f7b3fabf2b60bed1991ca8e13bbdf55d9785e1ba
MarkMa2003 <ke.mark.ma2021@gmail.com>
2022-06-22 17:47:52 -0700

    plotting

--- protein.py
+++ protein.py
@@ -1,18 +1,25 @@
 from Bio.PDB import *
 from matplotlib import pyplot as plt
 
+# initilization of the parser
 parser = MMCIFParser()
 structure = parser.get_structure("myFile","1dg3.cif")
+# get 1DG3 specific data from the structure
 chain = structure[0]['A']
 xdata = []
 ydata = []
 for atom in chain.get_atoms():
+    # filter the atoms based on the name
     if atom.get_name() == 'CA':
+        # get the coordinates of these atoms
         v=atom.get_vector()
+        # add the coordinates to the data to plot graph
         xdata.append(v[0])
         ydata.append(v[1])
 
+#plot the scatter graph using matplotlib
 plt.scatter(xdata,ydata)
-#mmcif_dict = MMCIF2Dict("1dg3.cif")
-#y_list = mmcif_dict["_atom_site.Cartn_y"]
-#print(list)
+plt.tight_layout()
+plt.xlabel('x-coordinate')
+plt.ylabel('y-coordinate')
+plt.show()
