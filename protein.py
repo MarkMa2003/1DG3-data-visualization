847157c1a00494bcf31ac25e03ca179581f53e39
MarkMa2003 <ke.mark.ma2021@gmail.com>
2022-06-22 17:24:07 -0700

    data visualization

--- protein.py
+++ protein.py
@@ -1,8 +1,18 @@
 from Bio.PDB import *
+from matplotlib import pyplot as plt
 
 parser = MMCIFParser()
 structure = parser.get_structure("myFile","1dg3.cif")
-chain = structure[1][1]
-chain.get_atoms()
-mmcif_dict = MMCIF2Dict("1dg3.cif")
-y_list = mmcif_dict["_atom_site.Cartn_y"]
+chain = structure[0]['A']
+xdata = []
+ydata = []
+for atom in chain.get_atoms():
+    if atom.get_name() == 'CA':
+        v=atom.get_vector()
+        xdata.append(v[0])
+        ydata.append(v[1])
+
+plt.scatter(xdata,ydata)
+#mmcif_dict = MMCIF2Dict("1dg3.cif")
+#y_list = mmcif_dict["_atom_site.Cartn_y"]
+#print(list)
